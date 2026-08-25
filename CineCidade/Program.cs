b378fbc7d8a7a489ea34a4d659cf511284381036
musashimbt <musashimbt@gmail.com>
2026-08-24 23:01:51 -0300

    feat: adiciona entrada dos dados do cliente

--- CineCidade/Program.cs
+++ CineCidade/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite o nome do cliente: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite a quantidade de ingressos: ");
+int quantidadeIngressos = int.Parse(Console.ReadLine()!);
+
+Console.Write("Digite o cupom de desconto: ");
+string cupom = Console.ReadLine()!;
+
+Console.Write("Digite a idade do cliente: ");
+int idade = int.Parse(Console.ReadLine()!);
